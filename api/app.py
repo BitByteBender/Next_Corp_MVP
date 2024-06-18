39e619b00c1f620d6a0dbfd1716bb35cfc4508c2
Soufiane Sadgali <ssadgali2@gmail.com>
2024-06-18 18:07:53 +0100

    Added status check and routes for corps structure

---
+++ api/app.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+from flask import Flask, jsonify
+from models import storage
+from api.views import app_views
+from os import getenv
+
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+
+@app.errorhandler(404)
+def trigger_error(err):
+    """ Triggers a 404 error """
+    return (jsonify({"error": "Not found"}), 404)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """ Close storage on teardown """
+    storage.close()
+
+
+if __name__ == '__main__':
+    """ Runs flask app on a specified adr and port """
+    host = getenv('NC_API_HOST', '0.0.0.0')
+    port = int(getenv('NC_API_PORT', '5000'))
+    app.run(host=host, port=port, threaded=True)
