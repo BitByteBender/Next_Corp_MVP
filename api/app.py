547cce99883ddef3e29582e838348c676b86d4a2
Soufiane Sadgali <ssadgali2@gmail.com>
2024-06-19 22:49:56 +0100

    Adding login and register for corps

--- api/app.py
+++ api/app.py
@@ -1,13 +1,25 @@
 #!/usr/bin/env python3
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 from models import storage
 from api.views import app_views
+import os
 from os import getenv
+from pages import view
+from auth.register import register
+from auth.login import login
+from dashboard import dash
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = getenv('SECRET_KEY', os.urandom(24))
+
+
 app.register_blueprint(app_views)
+app.register_blueprint(view)
+app.register_blueprint(register, url_prefix='/corp_auth')
+app.register_blueprint(login, url_prefix='/corp_auth')
+app.register_blueprint(dash, url_prefix='/admin')
 
 
 @app.errorhandler(404)
@@ -26,4 +38,4 @@ if __name__ == '__main__':
     """ Runs flask app on a specified adr and port """
     host = getenv('NC_API_HOST', '0.0.0.0')
     port = int(getenv('NC_API_PORT', '5000'))
-    app.run(host=host, port=port, threaded=True)
+    app.run(host=host, port=port, threaded=True, debug=True)
